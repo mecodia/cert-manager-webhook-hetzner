2ee1d36ffaf2d1296445c95871eda76919ab9a7e
Tobias Birmili <birmili@mecodia.de>
2023-01-23 11:02:24 +0100

    test: fix test setup

--- main_test.go
+++ main_test.go
@@ -16,11 +16,7 @@ func TestRunsSuite(t *testing.T) {
 		dns.SetResolvedZone(zone),
 		dns.SetAllowAmbientCredentials(false),
 		dns.SetManifestPath("testdata/hcloud-dns"),
-		//dns.SetDNSServer("127.0.0.1:59351"),
 		dns.SetStrict(true),
-		dns.SetUseAuthoritative(false),
 	)
-	//fixture.RunConformance(t)
-	fixture.RunBasic(t)
-	fixture.RunExtended(t)
+	fixture.RunConformance(t)
 }
